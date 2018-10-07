46538ec96beecf118ca661d8c89ceb03a48615fc
Anna Panichkina <annjellyiu5@gmail.com>
2018-10-07 19:36:05 +0300

    add indicator for climate data

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,6 @@
 import '../css/index.css';
-import getIconSrc from './icons'
 import data from '../mocks/events.json'
-
-function fillCardDataEl(type, data) {
-  const template = document.getElementsByTagName("template")[0];
-
-  if (type === "fridge" && data.buttons && data.buttons.length > 1) {
-    const controlsTmpl = template.content.querySelector("div.controls");
-    const buttonTmpl =  template.content.querySelector("button.button");
-    const controls = document.importNode(controlsTmpl, true);
-
-
-    for (let i = 0; i < data.buttons.length; i++) {
-      const button = document.importNode(buttonTmpl, true);
-      button.textContent = data.buttons[i];
-      controls.appendChild(button);
-    }
-
-    return controls
-  }
-
-  return null
-}
-
-function fillCard(card, content) {
-  const header = card.querySelector(".card__header");
-  const title = card.querySelector(".card__title");
-  const icon = card.querySelector(".card__icon");
-  const source = card.querySelector(".card__source");
-  const time = card.querySelector(".card__time");
-  const body = card.querySelector(".card__body");
-  const isCritical = content.type === 'critical';
-
-  if (isCritical) {
-    header.classList.add('card__header_red');
-  }
-
-  icon.src = getIconSrc(content.icon, isCritical);
-  title.textContent = content.title;
-  source.textContent = content.source;
-  time.textContent = content.time;
-  body.textContent = content.description;
-
-  if (content.data) {
-    const cardDataEl = fillCardDataEl(content.icon, content.data);
-
-    if (cardDataEl) {
-      body.appendChild(cardDataEl)
-    }
-  }
-
-  card.classList.add(`card_${content.size}`);
-}
+import fillCard from './cardTemplate'
 
 function setContent(parentEl) {
   const template = document.getElementsByTagName("template")[0];
