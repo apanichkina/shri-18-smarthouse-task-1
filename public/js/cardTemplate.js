0ae5173cd0622b3a45df46253387e0836e0691fa
Anna Panichkina <annjellyiu5@gmail.com>
2018-10-10 13:30:19 +0300

    refacktor chart

--- public/js/cardTemplate.js
+++ public/js/cardTemplate.js
@@ -48,8 +48,6 @@ function fillIndicator(container, data) {
 
 }
 
-
-
 function fillCardDataEl(type, data) {
   const template = TEMPLATE[1];
   const result = [];
@@ -160,66 +158,6 @@ export default function fillCard(card, content) {
       for (let i = 0; i < cardDataEl.length; i++) {
         body.appendChild(cardDataEl[i])
       }
-    }
-
-    const chrtContainer = document.getElementById("chart");
-    if (chrtContainer) {
-
-      const ctx = chrtContainer.getContext('2d');
-
-      // TODO destroy chart
-      var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          datasets: [{
-            label: 'electricity',
-            data: [
-              {x:"1536883200", y: 115},
-              {x:"1536969600", y: 117},
-            ],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          },
-            {
-              label: 'water',
-              data: [
-                {x:"1536883200", y: 40},
-                {x:"1536969600", y: 40.2},
-              ],
-              backgroundColor: [
-                'rgba(75, 192, 192, 0.2)',
-              ],
-              borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero:true
-              }
-            }]
-          }
-        }
-      });
     }
 
   }
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,77 @@
 import '../css/index.css';
 import data from '../mocks/events.json'
 import fillCard from './cardTemplate'
+import {Chart} from "chart.js";
+
+const chartBackgroundColor = {
+  'water': 'rgba(54, 162, 235, 0.2)',
+  'electricity': 'rgba(255, 206, 86, 0.2)',
+  'gas': 'rgba(255, 159, 64, 0.2)',
+  'default': 'rgba(255,99,132, 0.2)',
+};
+
+const chartBorderColor = {
+  'water': 'rgba(54, 162, 235, 1)',
+  'electricity': 'rgba(255, 206, 86, 1)',
+  'gas': 'rgba(255, 159, 64, 1)',
+  'default': 'rgba(255,99,132, 1)',
+};
+
+
+function prepareDataForChart(data) {
+  const defaultBackgroundColor = chartBackgroundColor['default'];
+  const defaultBorderColor = chartBorderColor['default'];
+
+  let result = [];
+
+  for (let item of data) {
+    Object.keys(item).forEach((key) => {
+      let localResult = {label: key, data: [], borderWidth: 1};
+
+      for (let param of item[key]) {
+        localResult.backgroundColor = chartBackgroundColor[key] || defaultBackgroundColor;
+        localResult.borderColor = chartBorderColor[key] || defaultBorderColor;
+        localResult.data.push({x: new Date(Number(param[0])), y: param[1]})
+      }
+      result.push(localResult)
+    });
+  }
+
+  return result;
+}
+
+function drawChart(container, data) {
+  console.log('fff', data)
+  const ctx = container.getContext('2d');
+
+  // TODO destroy chart
+  return new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: data
+    },
+    options: {
+      responsive: true,
+
+      scales: {
+        xAxes: [{
+          type: 'time',
+          time: {
+            displayFormats: {
+              minute: 'h:mm'
+            }
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
+}
+
 
 function setContent(parentEl) {
   const template = document.getElementsByTagName("template")[0];
@@ -12,6 +83,16 @@ function setContent(parentEl) {
     card = document.importNode(cardTmpl, true);
     fillCard(card, content[i]);
     parentEl.appendChild(card);
+
+
+    if (content[i].data && content[i].data.type === 'graph') {
+      const chartContainer = document.getElementById('chart')
+      const parsedData = prepareDataForChart(content[i].data.values)
+
+      if (parsedData.length && chartContainer) {
+        drawChart(chartContainer, parsedData)
+      }
+    }
   }
 }
 
