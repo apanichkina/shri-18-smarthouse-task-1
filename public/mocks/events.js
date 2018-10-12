70ea42ad61904062a5a671c0e5f479b7c608fc2e
Anna Panichkina <annjellyiu5@gmail.com>
2018-10-12 18:14:33 +0300

    update readme

--- public/mocks/events.js
+++ public/mocks/events.js
@@ -1,109 +1,6 @@
 export default {
   "events": [
     {
-      "type": "critical",
-      "title": "Пылесос застрял",
-      "source": "Сенсор движения",
-      "time": "16:17, Сегодня",
-      "description": "Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.",
-      "icon": "cam",
-      "data": {
-        "image": "get_it_from_mocks_:3.jpg"
-      },
-      "size": "l"
-    },
-    {
-      "type": "info",
-      "title": "Дверь открыта",
-      "source": "Сенсор входной двери",
-      "time": "18:50, Сегодня",
-      "description": null,
-      "icon": "key",
-      "size": "s"
-    },
-    {
-      "type": "info",
-      "title": "Уборка закончена",
-      "source": "Пылесос",
-      "time": "18:45, Сегодня",
-      "description": null,
-      "icon": "robot-cleaner",
-      "size": "s"
-    },
-    {
-      "type": "info",
-      "title": "Новый пользователь",
-      "source": "Роутер",
-      "time": "18:45, Сегодня",
-      "description": null,
-      "icon": "router",
-      "size": "s"
-    },
-    {
-      "type": "info",
-      "title": "Изменен климатический режим",
-      "source": "Сенсор микроклимата",
-      "time": "18:30, Сегодня",
-      "description": "Установлен климатический режим «Фиджи»",
-      "icon": "thermal",
-      "size": "m",
-      "data": {
-        "temperature": 24,
-        "humidity": 80
-      }
-    },
-    {
-      "type": "critical",
-      "title": "Невозможно включить кондиционер",
-      "source": "Кондиционер",
-      "time": "18:21, Сегодня",
-      "description": "В комнате открыто окно, закройте его и повторите попытку",
-      "icon": "ac",
-      "size": "m"
-    },
-    {
-      "type": "info",
-      "title": "Музыка включена",
-      "source": "Яндекс.Станция",
-      "time": "18:16, Сегодня",
-      "description": "Сейчас проигрывается:",
-      "icon": "music",
-      "size": "m",
-      "data": {
-        "albumcover": "https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000",
-        "artist": "Florence & The Machine",
-        "track": {
-          "name": "Big God",
-          "length": "4:31"
-        },
-        "volume": 80
-      }
-    },
-    {
-      "type": "info",
-      "title": "Заканчивается молоко",
-      "source": "Холодильник",
-      "time": "17:23, Сегодня",
-      "description": "Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?",
-      "icon": "fridge",
-      "size": "m",
-      "data": {
-        "buttons": [
-          "Да",
-          "Нет"
-        ]
-      }
-    },
-    {
-      "type": "info",
-      "title": "Зарядка завершена",
-      "source": "Оконный сенсор",
-      "time": "16:22, Сегодня",
-      "description": "Ура! Устройство «Оконный сенсор» снова в строю!",
-      "icon": "battery",
-      "size": "s"
-    },
-    {
       "type": "info",
       "title": "Еженедельный отчет по расходам ресурсов",
       "source": "Сенсоры потребления",
@@ -210,6 +107,109 @@ export default {
             ]
           }
         ]
+      },
+      "size": "l"
+    },
+    {
+      "type": "info",
+      "title": "Дверь открыта",
+      "source": "Сенсор входной двери",
+      "time": "18:50, Сегодня",
+      "description": null,
+      "icon": "key",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Уборка закончена",
+      "source": "Пылесос",
+      "time": "18:45, Сегодня",
+      "description": null,
+      "icon": "robot-cleaner",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Новый пользователь",
+      "source": "Роутер",
+      "time": "18:45, Сегодня",
+      "description": null,
+      "icon": "router",
+      "size": "s"
+    },
+    {
+      "type": "info",
+      "title": "Изменен климатический режим",
+      "source": "Сенсор микроклимата",
+      "time": "18:30, Сегодня",
+      "description": "Установлен климатический режим «Фиджи»",
+      "icon": "thermal",
+      "size": "m",
+      "data": {
+        "temperature": 24,
+        "humidity": 80
+      }
+    },
+    {
+      "type": "critical",
+      "title": "Невозможно включить кондиционер",
+      "source": "Кондиционер",
+      "time": "18:21, Сегодня",
+      "description": "В комнате открыто окно, закройте его и повторите попытку",
+      "icon": "ac",
+      "size": "m"
+    },
+    {
+      "type": "info",
+      "title": "Музыка включена",
+      "source": "Яндекс.Станция",
+      "time": "18:16, Сегодня",
+      "description": "Сейчас проигрывается:",
+      "icon": "music",
+      "size": "m",
+      "data": {
+        "albumcover": "https://avatars.yandex.net/get-music-content/193823/1820a43e.a.5517056-1/m1000x1000",
+        "artist": "Florence & The Machine",
+        "track": {
+          "name": "Big God",
+          "length": "4:31"
+        },
+        "volume": 80
+      }
+    },
+    {
+      "type": "info",
+      "title": "Заканчивается молоко",
+      "source": "Холодильник",
+      "time": "17:23, Сегодня",
+      "description": "Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?",
+      "icon": "fridge",
+      "size": "m",
+      "data": {
+        "buttons": [
+          "Да",
+          "Нет"
+        ]
+      }
+    },
+    {
+      "type": "info",
+      "title": "Зарядка завершена",
+      "source": "Оконный сенсор",
+      "time": "16:22, Сегодня",
+      "description": "Ура! Устройство «Оконный сенсор» снова в строю!",
+      "icon": "battery",
+      "size": "s"
+    },
+    {
+      "type": "critical",
+      "title": "Пылесос застрял",
+      "source": "Сенсор движения",
+      "time": "16:17, Сегодня",
+      "description": "Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь.",
+      "icon": "cam",
+      "data": {
+        "image": "get_it_from_mocks_:3.jpg"
       },
       "size": "l"
     },
